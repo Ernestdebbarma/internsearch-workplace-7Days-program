4cf9b158837b5c43ce478d40c379daa538bde8c6
Ernest Debbarma <72308267+Ernestdebbarma@users.noreply.github.com>
2020-10-06 18:21:22 +0530

    some questions ni ans.//from D1 to D2 and so on...

---
+++ practice questions.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int x=23,y=-11,z=2,p=-56,a=8,total;
+
+    total=x+y-2*z%p/2-a++;
+    printf("the total is %d",total);
+
+    return 0;
+
+}
+
+//another one and so on
+//4/a 
+ #include<stdio.h>
+int main()
+{
+   int a = 10/45*23%45/(45%4*21);
+
+   printf("%d",a);
+
+   return 0;
+}
+/* the ans comes 0 */
+
+// 4/b
+
+#include<stdio.h>
+int main()
+{
+  float a = 10+45.0*23-45+(4*21.0);
+
+   printf("%f",a);
+
+   return 0;
+}
+/* The answer comes 1084.000000*/
