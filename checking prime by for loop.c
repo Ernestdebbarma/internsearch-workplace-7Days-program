c6d900a21c3299dc81cc7e9c672cda9815668e27
Ernest Debbarma <72308267+Ernestdebbarma@users.noreply.github.com>
2020-10-07 19:00:25 +0530

    i have problem in it.

---
+++ checking prime by for loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a, prm,b;
+   printf("give the number you want to check for prime: ");
+   scanf("%d",&prm);
+
+   for(a=prm;a%prm==1;b++)
+
+    printf("%d is prime");
+
+
+    return 0;
+}
