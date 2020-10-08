3a95efa29f689f42cc73402f0e8ea35cbdc21532
Ernest Debbarma <72308267+Ernestdebbarma@users.noreply.github.com>
2020-10-08 16:45:15 +0530

    omg its so boring to do, wish i learned array

---
+++ alphabet checking by switch.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+
+    char nun;
+   printf("enter your alphabet: ");
+  scanf("%c",&nun);
+
+  switch(nun)
+    {
+       case 'a':
+            printf("Vowel");
+            break;
+        case 'e':
+            printf("Vowel");
+            break;
+        case 'i':
+            printf("Vowel");
+            break;
+        case 'o':
+            printf("Vowel");
+            break;
+        case 'u':
+            printf("Vowel");
+            break;
+        case 'A':
+            printf("Vowel");
+            break;
+        case 'E':
+            printf("Vowel");
+            break;
+        case 'I':
+            printf("Vowel");
+            break;
+        case 'O':
+            printf("Vowel");
+            break;
+        case 'U':
+            printf("Vowel");
+            break;
+        default:
+            printf("Consonant");
+    }
+
+   return 0;
+
+}
