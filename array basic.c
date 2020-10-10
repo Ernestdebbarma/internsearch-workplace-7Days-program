4a499cc6cf2980e8270e4400ebfa9708eb9bc1af
Ernest Debbarma <72308267+Ernestdebbarma@users.noreply.github.com>
2020-10-10 12:26:36 +0530

    just some basics on array

---
+++ array basic.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char hi[]="ernest_debbarma",jo[]="my arrays";
+    printf("%s\n",hi);
+    printf("%s\n",jo);
+    int gjs[5];
+    for(int i=0;i<=4;i++){
+        scanf("%d",&gjs[i]);
+    }
+    for(int i=0;i<=4;i++){
+        printf("\n%d\n",gjs[i]);
+    }
+
+    return 0;
+}
